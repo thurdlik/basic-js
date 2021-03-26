bd5bdb3da1a502b70ea6015b845c558d76789c57
Linar <>
2021-03-26 16:17:59 +0300

    feat: complete getSeason function

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (typeof(date) !== 'object') {
+    return 'Unable to determine the time of year!'; 
+  }
+  let month = date.getMonth();
+  if (month === 0 || month === 1 || month === 11) {
+    return 'Winter';
+  }
+  if (month === 2 || month === 3 || month === 4) {
+    return 'Spring';
+  }
+  if (month === 5 || month === 6 || month === 7) {
+    return 'Summer';
+  }
+  if (month === 8 || month === 9 || month === 10) {
+    return 'Autumn';
+  }
+}
