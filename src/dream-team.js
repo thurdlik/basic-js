9a7ab451d72c49cf9c3f2a672cd55e7ee651b50d
Linar <>
2021-03-26 14:04:18 +0300

    feat: create function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  var result = [];
+  for (i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      result.push(members[i].charAt(0));
+    }
+  }
+  return console.log(result.sort().join(''));
 };
+
+createDreamTeam(['Khayrut', true, 14, 'Leonid', 'Ilham', 'Husainov']);
