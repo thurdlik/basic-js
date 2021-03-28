2c23a12eb657b776f0ca4c69c174b07697604c3d
Linar <>
2021-03-28 16:13:16 +0300

    feat: complete what-season function

--- src/what-season.js
+++ src/what-season.js
@@ -4,17 +4,23 @@ module.exports = function getSeason(date) {
   if (typeof(date) !== 'object') {
     return 'Unable to determine the time of year!'; 
   }
+  let year = date.getFullYear();
   let month = date.getMonth();
+  let day = date.getDate();
+  if (Object.prototype.hasOwnProperty.call(date, "getMonth")) {
+    throw new TypeError;
+  }
+  
   if (month === 0 || month === 1 || month === 11) {
-    return 'Winter';
+    return 'winter';
   }
   if (month === 2 || month === 3 || month === 4) {
-    return 'Spring';
+    return 'spring';
   }
   if (month === 5 || month === 6 || month === 7) {
-    return 'Summer';
+    return 'summer';
   }
   if (month === 8 || month === 9 || month === 10) {
-    return 'Autumn';
+    return 'autumn';
   }
 }
